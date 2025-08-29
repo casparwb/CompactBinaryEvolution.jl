a154642c74aa3c15996410847775c35a427135d3
Caspar William Bruenech <c.w.bruenech@proton.me>
2025-08-29 11:52:55 +0200

    fixed a bunch of stuff

--- src/CompactBinaryEvolution.jl
+++ src/CompactBinaryEvolution.jl
@@ -51,6 +51,8 @@ function newtonian_angular_momentum(m1, m2, a, e::AbstractVector)
     newtonian_angular_momentum(m1, m2, a, norm(e))
 end
 
+
+
 struct CompactBinary{tT, mT, aT, eT, lT, ST}
     time::tT
     m1::mT
@@ -83,6 +85,58 @@ struct CompactBinary{tT, mT, aT, eT, lT, ST}
         lT = typeof(L)
         return new{tT, mT, aT, eT, lT, sT}(time, m1, m2, a, e, L, S1, S2)
     end
+end
+
+struct CompactBinarySolution{tT, aT, eT, evT, lT, sT}
+    ic::CompactBinary
+    time::tT
+    a::aT
+    e::eT
+    e_vec::evT
+    L::lT
+    S1::sT
+    S2::sT
+
+    function CompactBinarySolution(sol, binary)
+
+        u = Array(sol)
+
+        S1, S2 = if size(u, 2) == 5
+            u[:,3,:] .* unit_spin, u[:,4,:] .* unit_spin
+        else
+            nothing, nothing
+        end
+
+        L = u[:,:,end] .* unit_spin
+
+        a = u[1,1,:] .* unit_length
+        e_vec = u[:,2,:]
+        e = norm.(eachcol(e_vec))
+
+        time = sol.t .* unit_time
+
+        tT = typeof(time)
+        aT = typeof(a)
+        eT = typeof(e)
+        evT = typeof(e_vec)
+        sT = typeof(S1)
+        lT = typeof(L)
+
+        return new{tT, aT, eT, evT, lT, sT}(binary, time, a, e, e_vec, L, S1, S2)
+    end
+end
+
+function Base.show(io::IO, sol::CompactBinarySolution)
+    N = length(sol.time)
+    a_final = sol.a[end]
+    e_final = sol.e[end]
+    t_final = sol.time[end]
+
+    println("CompactBinarySolution: ")
+    println("  N: ", N)
+    println("  Final a [R⊙]: ", a_final)
+    println("  Final e: ", e_final)
+    println("  Final t [yr]: ", t_final)
 end
 
 include("equations.jl")
@@ -125,8 +179,6 @@ function get_u0(bin, ::SpinEvolutionModel)
     return [a0 e0 S1 S2 L]
 end
 
-get_a_e(u, model) = u[1, 1], norm(SA[u[1,2], u[2,2], u[3,2]])
-get_a_e(u, model::PetersModel) = u[1, 1], u[1, 2]
 
 function peak_f_GW(sqrt_GM_div_π::T, a::T, e::T)::T where T <: Real
     # (a < zero(a) || e > one(e)) && return 0.0
@@ -135,59 +187,6 @@ function peak_f_GW(sqrt_GM_div_π::T, a::T, e::T)::T where T <: Real
     return num/denom
 end
 
-# function get_merger_condition(bin, model::PetersModel; peak_frequency_stop=10.0u"Hz",
-#                                                       a_min_rg=10)
-#     GM = G*(bin.m1 + bin.m2)
-#     min_a = a_min_rg*GM/c^2
-
-#     sqrt_GM_div_π = sqrt(GM)/π
-#     sqrt_GM_div_π = ustrip(upreferred(unit(sqrt_GM_div_π)), sqrt_GM_div_π)
-#     get_a_e_(u) = get_a_e(u, model)
-
-#     peak_frequency_stop = ustrip(unit_time^-1, peak_frequency_stop)
-#     min_a = ustrip(unit_length, min_a)
-#     min_a = min_a
-#     function condition_merger!(out, u, t, integrator) 
-#         a, e = u[1, 1], u[1, 2]
-#         f_GW::Float64 = peak_f_GW(sqrt_GM_div_π, a, e)::Float64
-#         out[1] = (f_GW - peak_frequency_stop)::Float64
-#         out[2] = (a - min_a)::Float64
-#         nothing
-#     end
-
-#     return condition_merger!
-# end
-
-# set_out!(out, val, idx) = out[idx] = val
-
-# function get_merger_condition(bin, model; peak_frequency_stop=10.0u"Hz",
-#                                                       a_min_rg=10)
-#     GM = G*(bin.m1 + bin.m2)
-#     min_a = a_min_rg*GM/c^2
-
-#     sqrt_GM_div_π = sqrt(GM)/π
-#     sqrt_GM_div_π = ustrip(upreferred(unit(sqrt_GM_div_π)), sqrt_GM_div_π)
-#     # sqrt_GM_div_π::Float64 = Float64(sqrt_GM_div_π)
-#     get_a_e_(u) = get_a_e(u, model)
-
-#     peak_frequency_stop = ustrip(unit_time^-1, peak_frequency_stop)
-#     min_a = ustrip(unit_length, min_a)
-#     # min_a = min_a
-#     function condition_merger!(out, u, t, integrator) 
-#         a, e = u[1, 1], norm(SA[u[1, 2], u[2, 2], u[3, 2]])
-#         f_GW::Float64 = peak_f_GW(sqrt_GM_div_π, a, e)::Float64
-#         # out[1] = (f_GW - peak_frequency_stop)::Float64
-#         # out[2] = (a - min_a)::Float64
-#         out_freq::Float64 = f_GW - peak_frequency_stop
-#         out_a::Float64 = a - min_a
-#         set_out!(out, out_freq, 1)
-#         set_out!(out, out_a, 2)
-
-#         nothing
-#     end
-
-#     return condition_merger!
-# end
 
 function setup_callbacks(bin, model; merger_callback=true, saving_callback=false, save_every=1,
                               peak_frequency_stop=10.0u"Hz",
@@ -195,29 +194,21 @@ function setup_callbacks(bin, model; merger_callback=true, saving_callback=false
 
     cbs = []
 
-    GM = G*(bin.m1 + bin.m2)
-    min_a = a_min_rg*GM/c^2
-
+    GM = UNITLESS_G*ustrip(Float64, u"Msun", bin.m1 + bin.m2)
     sqrt_GM_div_π = sqrt(GM)/π
-    sqrt_GM_div_π = ustrip(upreferred(unit(sqrt_GM_div_π)), sqrt_GM_div_π)
-    # sqrt_GM_div_π::Float64 = Float64(sqrt_GM_div_π)
-    get_a_e_(u) = get_a_e(u, model)
-
-    peak_frequency_stop = ustrip(unit_time^-1, peak_frequency_stop)
-    min_a = ustrip(unit_length, min_a)
+    
+    min_a = a_min_rg*GM/UNITLESS_c^2
+    peak_frequency_stop = ustrip(Float64, unit_time^-1, peak_frequency_stop)
     if merger_callback
         function condition_merger!(out, u, t, integrator) 
-                a, e = u[1, 1], norm(SA[u[1, 2], u[2, 2], u[3, 2]])
-                f_GW::Float64 = peak_f_GW(sqrt_GM_div_π, a, e)::Float64
-                # out[1] = (f_GW - peak_frequency_stop)::Float64
-                # out[2] = (a - min_a)::Float64
-                out_freq::Float64 = f_GW - peak_frequency_stop
-                out_a::Float64 = a - min_a
-                # set_out!(out, out_freq, 1)
-                # set_out!(out, out_a, 2)
+
+                a = u[1, 1]
+                e = norm(SA[u[1, 2], u[2, 2], u[3, 2]])
+                f_GW = peak_f_GW(sqrt_GM_div_π, a, e)
+                out_freq = f_GW - peak_frequency_stop
+                out_a = a - min_a
                 out[1] = out_freq
                 out[2] = out_a
-
 
                 nothing
             end        
@@ -262,7 +253,8 @@ end
 function do_orbital_evolution(bin::CompactBinary, model;
                               t_final=13.8u"Gyr", dtype=Float64,
                               alg=Tsit5(),
-                              merger_callback=true, saving_callback=false, save_every=1,
+                              merger_callback=true, 
+                              save_every=Inf,
                               peak_frequency_stop=10.0u"Hz",
                               a_min_rg=10, verbose=false,
                               args...)
@@ -273,71 +265,66 @@ function do_orbital_evolution(bin::CompactBinary, model;
     tspan = ustrip.(dtype, unit_time, (bin.time, t_final))
     
     callback_args = Dict(:merger_callback => merger_callback, 
-                         :saving_callback => saving_callback, 
+                         :saving_callback => !isinf(save_every), 
                          :save_every => save_every,
                          :peak_frequency_stop => peak_frequency_stop,
                          :a_min_rg => a_min_rg, 
                          :verbose => verbose)
 
-    GM = G*(bin.m1 + bin.m2)
-    min_a = a_min_rg*GM/c^2
+    # GM = UNITLESS_G*ustrip(Float64, u"Msun", bin.m1 + bin.m2)
+    # sqrt_GM_div_π = sqrt(GM)/π
+    
+    # min_a = a_min_rg*GM/UNITLESS_c^2
+    # peak_frequency_stop = ustrip(Float64, unit_time^-1, peak_frequency_stop)
+    # function condition_merger!(out, u, t, integrator) 
 
-    sqrt_GM_div_π = sqrt(GM)/π
-    sqrt_GM_div_π = ustrip(upreferred(unit(sqrt_GM_div_π)), sqrt_GM_div_π)
-    # sqrt_GM_div_π::Float64 = Float64(sqrt_GM_div_π)
-    get_a_e_(u) = get_a_e(u, model)
+    #         a = u[1, 1]
+    #         e = norm(SA[u[1, 2], u[2, 2], u[3, 2]])
+    #         f_GW = peak_f_GW(sqrt_GM_div_π, a, e)
+    #         out_freq = f_GW - peak_frequency_stop
+    #         out_a = a - min_a
+    #         out[1] = out_freq
+    #         out[2] = out_a
 
-    peak_frequency_stop = ustrip(unit_time^-1, peak_frequency_stop)
-    min_a = ustrip(unit_length, min_a)
-    function condition_merger!(out, u, t, integrator) 
-            a::Float64 = u[1, 1]
-            e::Float64 = norm(SA[u[1, 2], u[2, 2], u[3, 2]])
-            f_GW::Float64 = peak_f_GW(sqrt_GM_div_π, a, e)::Float64
-            # out[1] = (f_GW - peak_frequency_stop)::Float64
-            # out[2] = (a - min_a)::Float64
-            out_freq::Float64 = f_GW - peak_frequency_stop
-            out_a::Float64 = a - min_a
-            # set_out!(out, out_freq, 1)
-            # set_out!(out, out_a, 2)
-            out[1] = out_freq
-            out[2] = out_a
-
-
-            nothing
-        end        
+    #         nothing
+    #     end        
         
-    function affect_merger!(integrator, idx) 
-        if idx == 1
-            verbose && println("Merger! (fGW)")
-            terminate!(integrator)
-        elseif idx == 2
-            verbose && println("Merger! (a)")
-            terminate!(integrator)
-        end
-        nothing
-    end
+    # function affect_merger!(integrator, idx) 
+    #     if idx == 1
+    #         verbose && println("Merger! (fGW)")
+    #         terminate!(integrator)
+    #     elseif idx == 2
+    #         verbose && println("Merger! (a)")
+    #         terminate!(integrator)
+    #     end
+    #     nothing
+    # end
 
-    cb_merger = VectorContinuousCallback(condition_merger!, affect_merger!, 2, 
-                                        save_positions=(false, false), 
-                                        rootfind=RightRootFind,
-                                        interp_points=100)
+    # cb_merger = VectorContinuousCallback(condition_merger!, affect_merger!, 2, 
+    #                                     save_positions=(false, false), 
+    #                                     rootfind=RightRootFind,
+    #                                     interp_points=100)
 
-    # callbacks = setup_callbacks(bin, model; callback_args...)
+    callbacks = setup_callbacks(bin, model; callback_args...)
     abstol = get(args, :abstol, 1e-6)
     reltol = get(args, :reltol, 1e-6)
-    maxiters = get(args, :maxiters, 250_000)
-    save_everystep = get(args, :save_everystep, !saving_callback)
+    dense = get(args, :dense, false)
+    maxiters = get(args, :maxiters, 100_000)
+    # save_everystep = get(args, :save_everystep, !saving_callback)
+    save_everystep = !haskey(args, :saveat)
+    save_everystep = !callback_args[:saving_callback]
 
     ode_func! = get_orbital_evolution_model(bin, model)
     prob = ODEProblem(ode_func!, u0, tspan)
     sol = solve(prob, alg; maxiters=maxiters, 
-                           callback=cb_merger,
+                           callback=callbacks,
                            abstol=abstol,
                            reltol=reltol, 
                            save_everystep=save_everystep,
+                           dense=dense,
                            args...)
 
-    return sol
+    return CompactBinarySolution(sol, bin)
 end
 
 
